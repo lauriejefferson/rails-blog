14fc830a8a241e1776ff30e3423ee77d6c934d81
ljdatasci <ljdatasci@gmail.com>
2022-12-11 16:23:49 -0500

    added redcarpet for markdown and styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+    def markdown(text)
+        extensions = {
+            autolink: true, 
+            no_intra_emphasis: true, 
+            fenced_code_blocks: true,
+            space_after_headers: true
+        }
+
+        render_options = {
+            hard_wrap: true,
+            escape_html: true 
+        }
+
+        renderer = Redcarpet::Render::HTML.new(render_options)
+        Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+
+    end
 end
